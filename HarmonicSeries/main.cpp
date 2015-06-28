100da8c00ce5937c97dd7a4fa3b2eaddf2d17211
Ivan Miskic <ivnmisk@gmail.com>
2015-06-28 14:23:01 +0200

    Multithreaded

--- HarmonicSeries/main.cpp
+++ HarmonicSeries/main.cpp
@@ -15,6 +15,9 @@ that's adding one number - then you just keep going like that for every new numb
 #include <ctime>
 #include <time.h> 
 #include <iomanip>
+#include <thread>
+
+std::vector<double> global_result;
 
 //Kahan summation algorithm
 double ksum(const std::vector<double> &data)
@@ -32,16 +35,17 @@ double ksum(const std::vector<double> &data)
 	return sum;
 }
 
-void sum()
+void sum(uint64_t iter)
 {
 	time_t start = time(0);
 	double time_passed;
 
 	double sum = 0;
-	double last_sum = 0;			 // Sum from previouse runs
-	double goal = 40;				 // Original goal was 40
-	unsigned int start_time = 0;	 // Time from previouse runs
-	uint64_t starting_iteration = 1; // Iteration previouse runs
+	double last_sum = 0;				 // Sum from previouse runs
+	double goal = 40;					 // Original goal was 40
+	unsigned int start_time = 0;		 // Time from previouse runs
+	uint64_t starting_iteration = (iter * 100000000U) - 100000000U + 1; // Iteration previouse runs
+	uint64_t end = iter * 100000000U;
 
 	std::vector<double> result;
 	std::vector<double> newResult;
@@ -49,9 +53,8 @@ void sum()
 	if (last_sum > 0)
 		result.push_back(last_sum);
 
-	
 	// accumulate iterable into partial sums
-	for (uint64_t i = starting_iteration; sum < goal; ++i)
+	for (uint64_t i = starting_iteration; i < end; ++i)
 	{
 		double x = 1.0 / (double)i;
 
@@ -72,20 +75,39 @@ void sum()
 		swap(result, newResult);
 
 		//print every 10 million iterations, adding new counter that counts to 10 mil and checking if its bigger than const 10mil would maybe be faster?
-		if (0U == (i % 10000000U))
+		/*if (0U == (i % 10000000U))
 		{
-			sum = ksum(result/*begin(result), end(result)*/);
-			time_passed = difftime(time(0), start) + start_time;
-			std::cout << std::fixed << std::setprecision(15) << "Iteration " << i << ": " << sum
-				<< "   " << std::setprecision(1) << "sec: " << time_passed
-				<< "  min: " << time_passed / 60
-				<< "  h: " << time_passed / 3600 << std::endl;
-		}
+		sum = ksum(result);
+		time_passed = difftime(time(0), start) + start_time;
+		std::cout << std::fixed << std::setprecision(15) << "Iteration " << 1 << ": " << sum
+		<< "   " << std::setprecision(1) << "sec: " << time_passed
+		<< "  min: " << time_passed / 60
+		<< "  h: " << time_passed / 3600 << std::endl;
+		}*/
+
 		newResult.clear();
 	}
+	//	global_result.push_back(ksum(result));
+	std::cout << "iter " << iter - 1 << ": " << ksum(result) << std::endl;
+	global_result[iter - 1] = ksum(result);
 }
+
 
 int main()
 {
-	sum();
+	size_t numOfThreads = std::thread::hardware_concurrency();
+	std::vector<std::thread> threads;
+	global_result.resize(numOfThreads);
+
+	std::cout << "Create " << numOfThreads << " threads..." << std::endl;
+	for (int i = 1; i <= numOfThreads; ++i)
+		threads.push_back(std::thread(sum, i));
+
+	std::cout << "Synchronizing all threads...\n";
+	for (auto& th : threads)
+		th.join();
+
+	for (int i = 0; i < numOfThreads; ++i)
+		std::cout << "Result of iteration(" << i << "): " << global_result[i] << std::endl;
+	std::cout << "\nGlobal Result: " << ksum(global_result) << std::endl;
 }
